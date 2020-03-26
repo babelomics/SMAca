fb028f08a2a88f86eca2e8f195c2b6640fdb4df6
Daniel López <daniel.lopez.lopez@juntadeandalucia.es>
2020-03-26 21:30:39 +0100

    add long description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.rst").read_text()
 
 setup(
     name='smaca',
@@ -9,8 +16,8 @@ setup(
     author='Daniel López López, Carlos Loucera',
     author_email=
     'daniel.lopez.lopez@juntadeandalucia.es, carlos.loucera@juntadeandalucia.es',
-    long_description=
-    'A python module for detecting spinal muscular atrophy carriers',
+    long_description=README,
+    long_description_content_type="text/x-rst",
     python_requires='>=3.6',
     install_requires=['click', 'cython', 'numpy', 'pysam', 'joblib'],
     include_package_data=True,
