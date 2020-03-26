bfb9169d3182d0b6ae4ac3f8da3db549b8c34d25
Carlos <loucerac@unican.es>
2020-03-26 18:24:58 +0100

    Add data folder for testing.

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup, find_packages
-import pathlib
-
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.rst").read_text()
+from setuptools import find_packages, setup
 
 setup(
     name='smaca',
@@ -15,18 +7,11 @@ setup(
     url='git@gitlab.cbra.com:dlopez/SMA_test.git',
     license='GNU General Public License v.3.0',
     author='Daniel López López, Carlos Loucera',
-    author_email='daniel.lopez.lopez@juntadeandalucia.es, carlos.loucera@juntadeandalucia.es',
-    long_description=README,
-    long_description_content_type="text/x-rst",
+    author_email=
+    'daniel.lopez.lopez@juntadeandalucia.es, carlos.loucera@juntadeandalucia.es',
+    long_description=
+    'A python module for detecting spinal muscular atrophy carriers',
     python_requires='>=3.6',
-    install_requires=[
-    	'click',
-        'cython',
-	    'numpy',
-	    'pysam',
-	    'joblib'
-	],
-    entry_points={
-        'console_scripts': ['smaca = smaca.cli:main']
-    }
-)
+    install_requires=['click', 'cython', 'numpy', 'pysam', 'joblib'],
+    include_package_data=True,
+    entry_points={'console_scripts': ['smaca = smaca.cli:main']})
