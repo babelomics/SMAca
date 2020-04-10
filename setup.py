aba1631829a0748dbd060282bcd57e9e55a42b5b
Daniel López <daniel.lopez.lopez@juntadeandalucia.es>
2020-04-10 23:21:25 +0200

    change version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.rst").read_text()
 
 setup(
     name='smaca',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(),
     url='https://github.com/babelomics/SMAca',
     license='GNU General Public License v.3.0',
